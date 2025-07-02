fac483d2a07af30e8d614b033e2a5db535f12c37
Dave <nedaroga@gmail.com>
2025-07-02 10:39:14 -0400

    New period in Opps

--- src/main_app.py
+++ src/main_app.py
@@ -11,12 +11,12 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 icon =  os.path.join(BASE_DIR, '..', 'images','logo.ico')
 
 
-opportunity_source_file = os.path.join(BASE_DIR, '..', 'data', 'Opportunities.csv')
+opportunity_source_file = os.path.join(BASE_DIR, '..', 'data', 'Opportunities.pkl')
 loeads_file =  os.path.join(BASE_DIR,'..', 'data', 'Clean_leads.csv')
 
 def load_opp():
     if not st.session_state.get('data_loaded', False):
-        st.session_state['opps'] = pd.read_csv(opportunity_source_file)
+        st.session_state['opps'] = pd.read_pkl(opportunity_source_file)
         st.session_state['opps'].columns = st.session_state['opps'].columns.str.strip()
         st.session_state['opps']['ValidFromDate'] = st.session_state['opps']['ValidFromDate'].astype('datetime64[ns]')
         st.session_state['opps']['ValidToDate'] = np.where(st.session_state['opps']['ValidToDate'] == '3000-01-01', '2250-01-01', st.session_state['opps']['ValidToDate'])
