d70767d7a84a458480a2bd1e5dcfb3fe5c3f488b
Dave <nedaroga@gmail.com>
2025-07-02 11:23:04 -0400

    New period in Opps

--- src/filter_opps.py
+++ src/filter_opps.py
@@ -7,12 +7,11 @@ def define_filters(data):
     period = st.sidebar.selectbox("Period", list(data['Period'].unique()))
     filtered_data = data[data['Period'] == period]
 
-
     start_date =  np.datetime64(period, 'D')
     end_date = np.datetime64(start_date + relativedelta(months=1))
 
     # Filter data based on date range
-    filtered_data = filtered_data[(filtered_data['ValidFromDate'] >= start_date) & (filtered_data['ValidFromDate'] <= end_date)]
+    filtered_data = filtered_data[(filtered_data['ValidFromDate'] >= start_date) & (filtered_data['ValidFromDate'] < end_date)]
 
     product = st.sidebar.selectbox("Product", options=['All'] + list(filtered_data['Product Family'].unique()))
     if product != 'All':
