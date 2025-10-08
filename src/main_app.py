a18eb967e9163d684bfdd614acfccd1573a1746c
Dave <nedaroga@gmail.com>
2025-10-08 16:54:28 -0400

    new structure

--- src/main_app.py
+++ src/main_app.py
@@ -14,6 +14,7 @@ opportunities_file_2025_5 =  os.path.join(BASE_DIR,'..', 'data', 'opportunities'
 opportunities_file_2025_6 =  os.path.join(BASE_DIR,'..', 'data', 'opportunities', 'opportunities_2025-06-01.csv')
 opportunities_file_2025_7 =  os.path.join(BASE_DIR,'..', 'data', 'opportunities', 'opportunities_2025-07-01.csv')
 opportunities_file_2025_8 =  os.path.join(BASE_DIR,'..', 'data', 'opportunities', 'opportunities_2025-08-01.csv')
+opportunities_file_2025_9 =  os.path.join(BASE_DIR,'..', 'data', 'opportunities', 'opportunities_2025-09-01.csv')
 
 file_2025_5 =  os.path.join(BASE_DIR,'..', 'data', 'leads','2025_5.csv')
 file_2025_6 =  os.path.join(BASE_DIR,'..', 'data', 'leads','2025_6.csv')
@@ -29,7 +30,8 @@ def load_opp():
             pd.read_csv(opportunities_file_2025_5),
             pd.read_csv(opportunities_file_2025_6),
             pd.read_csv(opportunities_file_2025_7),
-            pd.read_csv(opportunities_file_2025_8)
+            pd.read_csv(opportunities_file_2025_8),
+            pd.read_csv(opportunities_file_2025_9)
         ], ignore_index=True)
 
         st.session_state['opps'].columns = st.session_state['opps'].columns.str.strip()
