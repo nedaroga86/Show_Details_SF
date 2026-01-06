dc7949599c2b35c74a1c7f4b210ff081f89f2a77
Dave <nedaroga@gmail.com>
2026-01-06 12:28:00 -0500

    December data

--- src/main_app.py
+++ src/main_app.py
@@ -17,6 +17,7 @@ file_2025_8 =  os.path.join(BASE_DIR,'..', 'data', 'leads','2025_8.csv')
 file_2025_9 =  os.path.join(BASE_DIR,'..', 'data', 'leads','2025_9.csv')
 file_2025_10 =  os.path.join(BASE_DIR,'..', 'data', 'leads','2025_10.csv')
 file_2025_11 =  os.path.join(BASE_DIR,'..', 'data', 'leads','2025_11.csv')
+file_2025_12 =  os.path.join(BASE_DIR,'..', 'data', 'leads','2025_12.csv')
 
 
 def load_opp():
@@ -62,7 +63,9 @@ def load_leads():
         f_2025_10['Period'] = '2025-10-01'
         f_2025_11 = pd.read_csv(file_2025_11)
         f_2025_11['Period'] = '2025-11-01'
-        st.session_state['leads'] = pd.concat([f_2025_5, f_2025_6,f_2025_7,f_2025_8,f_2025_9,f_2025_10,f_2025_11])
+        f_2025_12 = pd.read_csv(file_2025_12)
+        f_2025_12['Period'] = '2025-12-01'
+        st.session_state['leads'] = pd.concat([f_2025_5, f_2025_6,f_2025_7,f_2025_8,f_2025_9,f_2025_10,f_2025_11,f_2025_11])
         st.session_state['leads_loaded'] = True
     return st.session_state['leads']
 
