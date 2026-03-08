7c7c915282bed337eb2a43e9ec88bb93321b09ac
Dave <nedaroga@gmail.com>
2026-03-08 15:58:26 -0400

    February 26 data

--- src/tables_leads.py
+++ src/tables_leads.py
@@ -23,17 +23,16 @@ def show_leads_table(leads_df, period):
     else:
         filtered_df = leads_df
 
-    st.dataframe(filtered_df, use_container_width=True, height=800)
 
     priority_mask = (
             (filtered_df['Special Exclusion'].isna()) &
             (pd.to_datetime(filtered_df['RFS Date/Time']) >= start_date) &
             (pd.to_datetime(filtered_df['RFS Date/Time']) < end_date) &
-            # (filtered_df['Marketing Source'] == 1) &
-            # (filtered_df['Name'] == 'Sales Generated Lead') &
-            # (filtered_df['Territory Bucket'].notna()) &
-            # (filtered_df['Territory Bucket'] != '') &
-            # (filtered_df['Territory Bucket'] != 'Non-RSD Total') &
+            (filtered_df['Marketing Source'] == 1) &
+            (filtered_df['Name'] == 'Sales Generated Lead') &
+            (filtered_df['Territory Bucket'].notna()) &
+            (filtered_df['Territory Bucket'] != '') &
+            (filtered_df['Territory Bucket'] != 'Non-RSD Total') &
             filtered_df['Priotity Exclusion'].isna()
     )
 
